eca0d877d4de62e4abe9d83b6ba2c5cf8afc383a
Markus Block <markus@block-net.de>
2025-06-03 22:39:58 +0200

    feat/update - added env value for dev environment - set swagger to only run on dev environment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins =
-      process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:4200';
+    process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:4200';
 
   app.enableCors({
     origin: allowedOrigins,
@@ -19,16 +19,19 @@ async function bootstrap() {
     preflightContinue: false,
   });
 
-  const config = new DocumentBuilder()
+  if (process.env.NODE_ENV !== 'prod') {
+    const config = new DocumentBuilder()
       .setTitle('PromptFlow API Documentation')
       .setDescription('This is the API documentation for PromptFlow')
       .setVersion('1.0')
       .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
       .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
